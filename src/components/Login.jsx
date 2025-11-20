697477f9f891d26ebf18073e360ccc7ab4d4233d
rafika khatun <rafikakhatun607@gmail.com>
2025-11-20 21:54:37 +0530

    feat(ui):add email input

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { User } from "lucide-react";
+import { Mail, User } from "lucide-react";
 import ParticlesBackground from "./ParticlesBackground";
 
 const Login = () => {
@@ -12,13 +12,24 @@ const Login = () => {
             <div className="relative z-10 flex justify-center items-center h-full">
                 <div className="bg-[#1D2666] p-6 rounded-xl shadow-xl border-gray-50 w-80">
                     <h2 className="text-xl font-semibold mb-4 text-center text-white drop-shadow ">Quick Chat with Rafika</h2>
-                    {/* */}
+                    <form action="" className="space-y-4">
+                        {/*UserName */}
                     <div className="relative w-full">
                         <label className="text-white" htmlFor="userName">Username</label>
-                        <input type="text" id="userName" className="w-full mt-2 px-4 py-2 rounded-md  bg-[#1D2666] outline-none shadow border border-gray-50 text-white " />
+                        <input type="text" id="userName" placeholder="Enter your name" className="w-full mt-2 px-4 py-2 rounded-md  bg-[#1D2666] outline-none shadow border border-gray-50 text-white " />
                          <User className="absolute top-10 right-3 items-center text-white" size={20} />
 
                     </div>
+
+                    {/*Email*/}
+                    <div className="relative w-full">
+                        <label className="text-white" htmlFor="email">Email</label>
+                        <input type="email" id="email" placeholder="Enter your email" className="w-full mt-2 px-4 py-2 rounded-md  bg-[#1D2666] outline-none shadow border border-gray-50 text-white " />
+                         <Mail className="absolute top-10 right-3 items-center text-white" size={20} />
+
+                    </div>
+                    </form>
+
 
                     {/* <input
             type="email"
