96c674b79092cb2680a3047e4c621ee7bf0a87db
rafika khatun <rafikakhatun607@gmail.com>
2025-11-20 20:57:21 +0530

    feat: initialize canvas, mouse, particles & animation refs using useRef

---
+++ src/components/Register.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ParticlesBackground from './ParticlesBackground';
+
+const Register = () => {
+    return (
+        <div>
+
+            <ParticlesBackground>
+            </ParticlesBackground>
+
+
+
+
+
+        </div>
+    );
+};
+
+export default Register;
