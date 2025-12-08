525841554ae971725abdf14feedf7e26869196d9
rafika khatun <rafikakhatun607@gmail.com>
2025-12-08 22:39:56 +0530

    feat: add route for Register  page

--- src/route/Router.jsx
+++ src/route/Router.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../components/Login";
+import Registration from "../components/Registration";
 
 const router =createBrowserRouter([
 
 {
     path:"/",
     element:<Login></Login>
-}
+},
+
+{
+    path:"/register",
+    element:<Registration></Registration>
+
+},
 
 
 
