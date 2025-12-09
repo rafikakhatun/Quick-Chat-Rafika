37da91987725d82cccb4e816dc794043109e2056
rafika khatun <rafikakhatun607@gmail.com>
2025-12-09 21:23:39 +0530

    feat(ui): add gradient pink-violet border and shadow enhancements to Registration form

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -4,8 +4,28 @@ import SpaceEffectRegisterPage from './SpaceEffectRegisterPage';
 const Registration = () => {
     return (
         <>
-        <SpaceEffectRegisterPage></SpaceEffectRegisterPage>
 
+            <div className='relative w-full h-screen overflow-hidden'>
+                <SpaceEffectRegisterPage></SpaceEffectRegisterPage>
+
+                {/* Register Form */}
+
+                <div className='relative h-full z-10 flex justify-center items-center'>
+                    <div className="p-8 rounded-xl shadow-xl max-w-md w-full" style={{
+                        backdropFilter: 'blur(1px)',
+                        backgroundColor: 'rgba(13, 6, 35, 0.65)',
+                        border: '1px solid rgba(138, 43, 226, 0.3)',
+                        boxShadow: '0 10px 25px rgba(0, 0, 0, 0.5), 0 0 15px rgba(138, 43, 226, 0.3)'
+                    }}>
+                        
+
+
+                    </div>
+
+                </div>
+
+
+            </div>
 
         </>
     );
