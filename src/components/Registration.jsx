9584d372cf328f0fae4231cd4cf7715b84c8c574
rafika khatun <rafikakhatun607@gmail.com>
2025-12-09 20:35:49 +0530

    create SpaceEffectRegisterPage file

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Registration = () => {
     return (
         <>
-<h1>hello</h1>
+<div className='w-full h-screen bg-violet-700 flex justify-center items-center'>
+    <h1 className='text-4xl text-white'>Hello I am Register page</h1>
+</div>
 
         </>
     );
---
+++ src/components/SpaceEffectRegisterPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SpaceEffectRegisterPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SpaceEffectRegisterPage;
